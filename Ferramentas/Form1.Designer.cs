2c31e8149a874802a34472535a99756d0482b8dc
douglas34 <douglas@cedet.com.br>
2021-03-12 12:51:14 -0300

    botoes vr escondidos

--- Ferramentas/Form1.Designer.cs
+++ Ferramentas/Form1.Designer.cs
@@ -110,6 +110,7 @@
             this.button1.TabIndex = 4;
             this.button1.Text = "Restaurar BD \"Suspect\"";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
@@ -124,6 +125,7 @@
             this.button2.TabIndex = 5;
             this.button2.Text = "Arrumar CEST";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
             // 
             // panel2
             // 
